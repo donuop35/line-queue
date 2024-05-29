3f87caf545a380c539e05dfd2271f9d377ca3c7c
羅東榮 <donuop1995@gmail.com>
2024-05-30 00:32:01 +0800

    feat: Line Bot api範例程式

--- config/line.php
+++ config/line.php
@@ -2,7 +2,8 @@
 
 return [
     'channel_id' => env('LINE_CHANNEL_ID'),
-    'secret' => env('LINE_SECRET'),
+    'channel_secret' => env('LINE_CHANNEL_SECRET'),
+    'access_token' => env('LINE_ACCESS_TOKEN'),
     'authorize_url'=>'https://access.line.me/oauth2/v2.1/authorize',
     'get_token_url' => 'https://api.line.me/oauth2/v2.1/token',
     'get_user_profile_url' => 'https://api.line.me/v2/profile',
